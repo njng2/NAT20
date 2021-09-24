e3a1b5f3179609e8b139617fe6d380152bea1097
thegabbythemaster <gabegabby99@gmail.com>
2021-09-24 14:16:58 -0400

    deleted active, staff, and added DOB in user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, name, password=None): #allows you to create user 
@@ -16,7 +16,7 @@ class UserAccountManager(BaseUserManager):
 
         return user
 
-class UserAccount(AbstractBaseUser, PermissionMixin):
+class UserAccount(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length = 255, unique = True)
     name = models.CharField(max_length = 255)
     is_active = models.BooleanField(default = True)
