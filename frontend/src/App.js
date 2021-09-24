8364a60bb274785e0701e44979fec4720f2fdada
armankabir <47779642+armankabir@users.noreply.github.com>
2021-09-24 15:05:30 -0400

    React Setup
    Setup components, containers, reducers, and assets needed for front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import  { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
-function App() {
+
+//Importing Containers
+import Home from './containers/Home';
+import Login from './containers/Login';
+import Signup from './containers/Signup';
+import Activate from './containers/Activate';
+import ResetPassword from './containers/ResetPassword';
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm';
+import Layout from './hocs/Layout';
+
+const App = () => {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+        <Layout>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                
+            </Switch>
+        </Layout>
+    </Router>
+  )
 }
 
 export default App;
