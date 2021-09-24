8195ede3bb1f6afaa07b7afe41040819e2a2321d
thegabbythemaster <gabegabby99@gmail.com>
2021-09-24 15:52:14 -0400

    setting up postman

--- backend/auth_system/urls.py
+++ backend/auth_system/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path(r'^auth/', include('djoser.urls')),
-    path(r'^auth/', include('djoser.urls.jwt')),
+    path('^auth/', include('djoser.urls')),
+    path('^auth/', include('djoser.urls.jwt')),
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
