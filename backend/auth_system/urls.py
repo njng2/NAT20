ae4a12ed4fd85d7953cb62c11610acd61bfe30cc
thegabbythemaster <gabegabby99@gmail.com>
2021-09-24 15:27:03 -0400

    created Djoser model for setting yo user requirements

--- backend/auth_system/urls.py
+++ backend/auth_system/urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path(r'^auth/', include('djoser.urls')),
     path(r'^auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name = 'index.html'))]
