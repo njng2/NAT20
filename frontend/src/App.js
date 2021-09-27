8d384279e80db7db3e261b569d6bda4a841374eb
armankabir <47779642+armankabir@users.noreply.github.com>
2021-09-26 22:26:08 -0400

    Added Login Functionality (testing required)
    Added ability to log in through front end. Further testing required to ensure log in is successful.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import  { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 //Importing Containers
 import Home from './containers/Home';
@@ -14,14 +16,26 @@ import Layout from './hocs/Layout';
 const App = () => {
  
   return (
-    <Router>
-        <Layout>
-            <Switch>
-                <Route exact path='/' component={Home} />
-                
-            </Switch>
-        </Layout>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <Layout>
+                <Switch>
+                    {/* Home Route */}
+                    <Route exact path='/' component={Home} /> 
+                    {/* Log-in Route */}
+                    <Route exact path='/login' component={Login} />
+                    {/* Sign-up Route */}
+                    <Route exact path='/signup' component={Signup} />
+                    {/* Reset Password Route */}
+                    <Route exact path='/reset-password' component={ResetPassword} />
+                    {/* Reset Password Confirmation Route */}
+                    <Route exact path='/password/reset/confirm/:uid/:token' component={ResetPasswordConfirm} />
+                    {/* Activate Account Route */}
+                    <Route exact path='/activate/:uid/:token' component={Activate} />
+                </Switch>
+            </Layout>
+        </Router>
+    </Provider>
   )
 }
 
--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -1,11 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Login = () => {
+import { login } from '../actions/auth';
+
+
+const Login = ({ login }) => {
+   /**  Information required to Login
+    * @formData : State, object holding form email and password
+    * @setFormData : State modifier function 
+    */
+   const [formData, setFormData] = useState({
+       email: '',
+       password: ''
+   });
+
+   //Setting email and password of formData
+   const { email, password } = formData;
+
+   //onChange function needed to change state from user input
+   const onChange = e => setFormData({...formData, [e.target.name]: e.target.value });
+
+   const onSubmit = e => {
+       e.preventDefault();
+       login(email. password)
+   }
+
+   //if user is authenticate, redirect them to the home page
+
     return (
-        <div>
-            Login
+        <div className="container mt-5">
+            <h1>Sign In</h1>
+            <p>Sign into your account</p>
+
+            {/* Call onSubmit function to login on submit of form*/}
+            <form onSubmit={e => onSubmit(e)}>
+                {/* Email Form */}
+                <div className='form-group'>
+                    <input
+                        className='form-control'
+                        type='email'
+                        placeholder='Email'
+                        name='email'
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
+                </div>
+                {/* Password Form */}
+                <div className='form-group'>
+                    <input
+                        className='form-control'
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        value={password}
+                        onChange={e => onChange(e)}
+                        minLength='6'
+                        required
+                    />
+                </div>
+                <button className='btn btn-primary' type='submit'>Login</button>
+            </form>
+            <p className='mt-3'>
+                Don't have an account? <Link to='/signup'>Sign Up Here.</Link>
+            </p>
+            <p className='mt-3'>
+                Forgot your password? <Link to='/signup'>Reset Password Here.</Link>
+            </p>
         </div>
     )
-}
+};
 
-export default Login;
+// const mapStateToProps = state => ({
+//     //Is authenticated?
+// });
+
+export default connect(null, { login } )(Login);
