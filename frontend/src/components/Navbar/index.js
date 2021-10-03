a4387c6c437544dfc9721f083926df8c3fddb921
thegabbythemaster <gabegabby99@gmail.com>
2021-10-03 13:32:25 -0400

    completed implementation of navbar

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,11 +1,39 @@
-import React from 'react'
+
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+      <>
+        <Nav>
+          <NavLink to='/'>
+            <h1>Logo</h1>
+          </NavLink>
+          <Bars />
+          <NavMenu>
+            <NavLink to='/about' activeStyle>
+              About
+            </NavLink>
+            <NavLink to='/home' activeStyle>
+              Home
+            </NavLink>
+            <NavLink to='/login' activeStyle>
+              Login
+            </NavLink>
+          </NavMenu>
+          <NavBtn>
+            <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+          </NavBtn>
+        </Nav>
+      </>
     );
-};
+  };
 
 export default Navbar;
--- frontend/src/hocs/Layout.js
+++ frontend/src/hocs/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
+//import { Router } from 'react-router';
 import Navbar from '../components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 function Layout(){
     return (
-        <>
+        <Router>
             <Navbar />
-        </>
+        </Router>
     )
 }
 export default Layout;
