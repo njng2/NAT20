c4ae07e145458c5724bdc3b87654ba8e19e1640b
thegabbythemaster <gabegabby99@gmail.com>
2021-10-03 12:36:44 -0400

    started navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,11 @@
-import React from 'react';
+// import React from 'react';
 
-const Navbar = () => {
-    return (
-        <div>
-            Navbar
-        </div>
-    )
-}
+// const Navbar = () => {
+//     return (
+//         <div>
+//             Navbar
+//         </div>
+//     )
+// }
 
-export default Navbar;
+// export default Navbar;
