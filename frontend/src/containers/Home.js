3c7944ed3cf88429e15791d16c5f8500db3fb094
Alfonso <argunwan@gmail.com>
2021-10-03 21:52:04 -0400

    edited the home page

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Home = () => {
     return (
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
-            <h1>Home</h1>
+            <div style = {{overflow:'hidden',height:'90vh', paddingRight: '-10vh', paddingLeft: '-100vh', background: 'absolute', alignContent: 'center'}}>
+            <div classname = "background-image" >
+            <h1 style = {{color: '#FFFFFF', textAlign: 'center', alignItems:'center', position: 'absolute'}}> "Welcome Adventurers!" </h1>
+            <p>
+            <img src="https://wallpaperaccess.com/full/117898.jpg"/>
+            </p>
+            </div>
+            </div>
         </div>
     )
 }
