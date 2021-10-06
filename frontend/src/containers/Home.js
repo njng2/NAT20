485150c1364b574a745f5d1027140ff23c86b90b
Alfonso <argunwan@gmail.com>
2021-10-06 16:10:11 -0400

    merge homepage

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -13,6 +13,16 @@ const Home = () => {
         <>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
+            <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
+            <div style = {{overflow:'hidden',height:'90vh', paddingRight: '-10vh', paddingLeft: '-100vh', background: 'absolute', alignContent: 'center'}}>
+            <div classname = "background-image" >
+            <h1 style = {{color: '#FFFFFF', paddingLeft: '480px', textAlign: 'center', alignItems:'center', position: 'absolute'}}> "Welcome Adventurers!" </h1>
+            <p>
+            <img src="https://wallpaperaccess.com/full/117898.jpg"/>
+            </p>
+            </div>
+            </div>
+        </div>
             
         </>
     )
