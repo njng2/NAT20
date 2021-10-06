b0de5cf0d9263a37aa375478ad49daf37ad4ece3
Alfonso <argunwan@gmail.com>
2021-10-06 15:49:00 -0400

    home page start up

--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
         <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh'}}>
             <div style = {{overflow:'hidden',height:'90vh', paddingRight: '-10vh', paddingLeft: '-100vh', background: 'absolute', alignContent: 'center'}}>
             <div classname = "background-image" >
-            <h1 style = {{color: '#FFFFFF', textAlign: 'center', alignItems:'center', position: 'absolute'}}> "Welcome Adventurers!" </h1>
+            <h1 style = {{color: '#FFFFFF', paddingLeft: '480px', textAlign: 'center', alignItems:'center', position: 'absolute'}}> "Welcome Adventurers!" </h1>
             <p>
             <img src="https://wallpaperaccess.com/full/117898.jpg"/>
             </p>
