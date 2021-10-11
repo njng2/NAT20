ccfd25ea1d0f20aa2ca887afdb2f3ef3de283a7a
thegabbythemaster <gabegabby99@gmail.com>
2021-10-11 13:23:39 -0400

    chat room server is up and running

--- backend/auth_system/urls.py
+++ backend/auth_system/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-
+from chat.views import index
 
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     #to get json token path
     path('auth/', include('djoser.urls.jwt')),
+    path('', index, name='index')
 ]
 
 
