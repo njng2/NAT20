a52cb337e76cb10aedb1e39f67a455dc0e4579d1
thegabbythemaster <gabegabby99@gmail.com>
2021-10-14 11:52:11 -0400

    adding message model

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
