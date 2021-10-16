a5e3a9ca58f1438395ff64d343718f20ce75a482
thegabbythemaster <gabegabby99@gmail.com>
2021-10-16 11:18:51 -0400

    try to added models

--- backend/chat/models.py
+++ backend/chat/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
 
@@ -7,3 +7,9 @@ class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_messages():
+        return Message.objects.order_by('-timestamp').all()[:10] 
