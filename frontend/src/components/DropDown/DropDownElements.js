8b698a5163387f70ec6803feb55d258177d517bd
Alfonso <argunwan@gmail.com>
2021-10-17 19:25:33 -0400

    pushing new changes

--- frontend/src/components/DropDown/DropDownElements.js
+++ frontend/src/components/DropDown/DropDownElements.js
@@ -1,3 +1,29 @@
 import styled from "styled-components";
+import background from "../../media/images/buildsbg2.png";
 
-//Add the styling components for the menu here as well
+export const BackgroundContainer = styled.div`
+    background-image: url(${background});
+    background-repeat: no-repeat;
+    background-size: cover;
+    justify-content: center;
+    width: 100wh;
+    height: 100vh;
+    color: white;
+    overflow: hidden;
+`
+
+export const DropDownStyle = styled.div`
+    width: 10vw;
+    height: 10vh;
+    postion: absolute;
+    text-color: red;
+    color: black;
+`
+export const BuildTitle = styled.div`
+ display: flex;
+ justify-content: center;
+ height: 90vh;
+ color: white; 
+ overflow: hidden;
+ padding-top: 25px;
+`
--- frontend/src/components/DropDown/index.js
+++ frontend/src/components/DropDown/index.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 import Select from 'react-select';
 import { Button } from 'react-scroll';
-
+import { BackgroundContainer, BuildTitle, DropDownStyle } from './DropDownElements';
 //Add the Components for the drop down elements here 
 //Basically the CSS components
+
+const DropDown = () => {
+    const data = [
+        {value: 1, label: "Dragonborn"},{ value: 2,label: "Dwarf"},{value: 3,label: "Elf"},
+        {value: 4,label: "Genassi"},{value: 5,label: "Grung"},{value: 6,label: "Locathah"}
+      ];
+   
+    const [selectedValue, setSelectedValue] = useState(0);
+    const handleChange = e => {
+        setSelectedValue(e.value);
+    }
+
+    return(
+        <BackgroundContainer>
+           <BuildTitle>
+            <h1>Character Builder</h1>
+            <DropDownStyle>
+            <br /><br />
+            <a style = {{color: 'white'}}>Race</a>
+
+            <Select
+                placeholder="Select"
+                value = {data.find(obj => obj.value === selectedValue)} // set selected value
+                options = {data} // set list of the data
+                onChange = {handleChange} // assign onChange function
+            />
+
+            <br /><br />
+                </DropDownStyle>
+           </BuildTitle>
+        </BackgroundContainer>
+    )
+}
+export default DropDown
