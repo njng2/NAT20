a94de8413f3832c434347585a58fa90c954f0f45
thegabbythemaster <gabegabby99@gmail.com>
2021-10-19 13:00:27 -0400

    message form

--- frontend/src/components/ChatSection/ChatFeed.jsx
+++ frontend/src/components/ChatSection/ChatFeed.jsx
@@ -1,10 +1,55 @@
-const ChatFeed = (props) => {
-    console.log(props);
+import MessageForm from "./MessagForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
 
-    return(
-        <div>
-            ChatFeed your at 16:26
+const ChatFeed = (props) => {
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat]
+
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+
+        return keys.map((key, index) => {
+            const message = messages[key];
+            const lastMessageKey = index === 0 ? null : keys[index - 1];
+            const isMyMessage = userName === message.sender.userName;
+
+            return (
+                <div key={`msg_${index}`} style={{width: '100%'}}>
+                    <div className="message-block">
+                        {
+                            isMyMessage
+                            ? <MyMessage message={message}/>
+                            : <TheirMessage message={message} lastMessage={messages[lastMessageKey]}/>
+                        }
+                        <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px': '68px'}}>
+                            read-receipts
+                        </div>
+
+                    </div>
+                </div>
+            );
+
+        })
+    }
+
+    if(!chat) return 'Loading...';
+
+    return (
+        <div className="chat-feed">
+          <div className="chat-title-container">
+            <div className="chat-title">{chat?.title}</div>
+            <div className="chat-subtitle">
+              {chat.people.map((person) => ` ${person.person.username}`)}
+            </div>
+          </div>
+          {renderMessages()}
+          <div style={{ height: '100px' }} />
+          <div className="message-form-container">
+            <MessageForm {...props} chatId={activeChat} />
+          </div>
         </div>
-    );
-}
+      );
+    };
 export default ChatFeed;
---
+++ frontend/src/components/ChatSection/MessagForm.jsx
@@ -0,0 +1,26 @@
+import { useState  } from "react";
+
+const MessageForm = () => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = () => {
+
+    }
+
+    const handleChange = () => {
+
+    }
+    return (
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input
+                className = "message-input"
+                placeholder = "Send a message..."
+                value = {value}
+                onChange = {handleChange}
+                onSubmit= {handleSubmit}
+             />
+        </form>
+    );
+}
+
+export default MessageForm;
