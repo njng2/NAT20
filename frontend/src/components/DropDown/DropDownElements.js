ac2e044eb12595122dddd8ac14a08c4b46f4eb97
Alfonso <argunwan@gmail.com>
2021-10-21 21:36:24 -0400

    added new styling

--- frontend/src/components/DropDown/DropDownElements.js
+++ frontend/src/components/DropDown/DropDownElements.js
@@ -16,14 +16,22 @@ export const DropDownStyle = styled.div`
     width: 10vw;
     height: 10vh;
     postion: absolute;
-    text-color: red;
     color: black;
 `
 export const BuildTitle = styled.div`
- display: flex;
- justify-content: center;
- height: 90vh;
- color: white; 
- overflow: hidden;
- padding-top: 25px;
+    display: flex;
+    justify-content: center;
+    height: 90vh;
+    color: white; 
+    overflow: hidden;
+    padding-top: 25px;
 `
+export const RaceTextBox = styled.div`
+    position: absolute;
+    text-color: black;
+    background-color: #000;
+    color: #fff;
+    opacity: .5;
+    justify-content: center;
+`
+
