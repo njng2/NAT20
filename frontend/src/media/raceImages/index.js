287583c453367d0db48f4136bbf0ebd3b8f6a40e
ifteASC4 <ifteahmed2000@gmail.com>
2021-10-30 17:06:31 -0400

    added two images to race page

---
+++ frontend/src/media/raceImages/index.js
@@ -0,0 +1,9 @@
+import dragonborn from './dragonborn.png'
+import halfelf from './half-elf.png'
+
+const imgMap = 
+{
+    'dragonborn':dragonborn,
+    'half-elf':halfelf
+};
+export default imgMap;
