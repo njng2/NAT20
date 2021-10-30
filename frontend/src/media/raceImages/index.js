d501e9ae979ad2336380417c67965175e4264624
ifteASC4 <ifteahmed2000@gmail.com>
2021-10-30 17:18:47 -0400

    added remaining images

--- frontend/src/media/raceImages/index.js
+++ frontend/src/media/raceImages/index.js
@@ -1,9 +1,23 @@
 import dragonborn from './dragonborn.png'
 import halfelf from './half-elf.png'
+import dwarf from './dwarf.png'
+import elf from './elf.png'
+import gnome from './gnome.png'
+import halforc from './half-orc.png'
+import halfling from './halfling.png'
+import tiefling from './tiefling.png'
+import human from './human.png'
 
 const imgMap = 
 {
     'dragonborn':dragonborn,
-    'half-elf':halfelf
+    'half-elf':halfelf,
+    'dwarf':dwarf,
+    'human':human,
+    'tiefling':tiefling,
+    'halfling':halfling,
+    'half-orc':halforc,
+    'gnome':gnome,
+    'elf':elf
 };
 export default imgMap;
