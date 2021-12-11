34c8feb691970ff8f9c3fc7818406dfd7afb12db
armankabir <47779642+armankabir@users.noreply.github.com>
2021-12-11 18:56:21 -0500

    Delete request now working from backend

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -5,6 +5,7 @@ from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from . models import *
 from rest_framework.response import Response
+from rest_framework import status
 from . serializers import *
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -26,6 +27,7 @@ class ReactView(APIView):
         "CHA" : detail.CHA,
         } 
         for detail in UsersHeroes.objects.all()]
+        # print(UsersHeroes.objects.get(id=3))
         return Response(detail)
   
     @csrf_exempt
@@ -37,24 +39,40 @@ class ReactView(APIView):
     
   
 class ReactDetail(APIView):
-
-    @csrf_exempt
-    def get_object(self, pk):
-        try:
-            return UsersHeroes.objects.get(id=pk)
-        except UsersHeroes.DoesNotExist:
-            raise Http404
+    serializer_class = UsersHeroesSerializer
+    # @csrf_exempt
+    # def get_object(self, pk):
+    #     try:
+    #         return UsersHeroes.objects.get(id=pk)
+    #     except UsersHeroes.DoesNotExist:
+    #         raise Http404
 
     @csrf_exempt
     def get(self, request, pk, format=None):
-        hero = UsersHeroes.objects.get(id=pk)
-        serializer = UsersHeroesSerializer(hero, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return  Response(serializer.data)
+        print("The pk is: ", pk)
+        userHero= UsersHeroes.objects.get(id=pk)
+        hero = {
+            "name": userHero.name,
+            "race_type": userHero.race_type, 
+            "class_type": userHero.class_type,
+            "STR" : userHero.STR, 
+            "DEX" : userHero.DEX,
+            "CON" : userHero.CON,
+            "INT" : userHero.INT,
+            "WIS" : userHero.WIS,
+            "CHA" : userHero.CHA,
+        }
+        return Response(hero)
+        # serializer = UsersHeroesSerializer(hero, data=request.data)
+        # if serializer.is_valid(raise_exception=True):
+        #     serializer.save()
+        #     return  Response(serializer.data)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     @csrf_exempt
     def delete(self, request, pk, format=None):
+        print("Here is delete")
         hero = UsersHeroes.objects.get(id=pk)
         hero.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
                 
