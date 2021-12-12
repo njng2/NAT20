45d3b1b23b088d1f126fb3f9009ac62ed759a90e
Alfonso <argunwan@gmail.com>
2021-12-11 23:09:51 -0500

    added bulleted lists to class text box

--- frontend/src/components/BuildLibPage/index.js
+++ frontend/src/components/BuildLibPage/index.js
@@ -9,6 +9,7 @@ import imgMap from '../../media/raceImages/index';
 import { makeStyles } from '@mui/styles';
 import { buttonBaseClasses } from '@mui/material';
 import Axios from 'axios';
+
 const useStyles = makeStyles({ //this dont work ?? idk
     media: {
       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
