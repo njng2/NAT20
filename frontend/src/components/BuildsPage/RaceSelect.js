e725a0e0e3bb0803d12889762d897d9e5ef54f22
ifteASC4 <ifteahmed2000@gmail.com>
2021-12-12 18:19:56 -0500

    made race component responsive

--- frontend/src/components/BuildsPage/RaceSelect.js
+++ frontend/src/components/BuildsPage/RaceSelect.js
@@ -14,144 +14,130 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 
 function TabPanel(props) {
-    const { children, value, index, ...other } = props;
+  const { children, value, index, ...other } = props;
   
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
   
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+  
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`
   };
-  
-  function a11yProps(index) {
-    return {
-      id: `simple-tab-${index}`,
-      "aria-controls": `simple-tabpanel-${index}`
-    };
-  }
-
-    
-    
+}
 
 
 const RaceSelect = ({selectedRace, handleChange, race, raceOptions}) => {
-const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState(0);
 
-const tabChange = (event, newValue) => {
-      setValue(newValue);
-};
+  const tabChange = (event, newValue) => {
+        setValue(newValue);
+  };
 
 
-    return (
-        <BuildTitle style>
-            <Container>
-                <Box sx={{ flexGrow: 1,marginTop:10 }}>
-                    <Grid container spacing = {10} alignItems="stretch">
-                    <Grid item xs={4} columns={18}> 
+  return (
+      <BuildTitle>
+          <Container>
+            <Box sx={{ flexGrow: 1,marginTop:10 }}>
+              <Grid container spacing = {8}alignItems="stretch">
+                <Grid item xs={15} sm={4}> 
+                  <a style={{ color: 'white' }}>Race</a>
 
-                    </Grid>
-                    <Grid item xs={2}>
-                    <h1>
-                    {
-                        selectedRace != 'none'?
-                        <RaceImageBox>
-                        <img src={imgMap[race.index]}></img> 
-                        </RaceImageBox>
-                        : null
-                    }   
-                    </h1>
-                    </Grid>
+                  <select value={selectedRace} onChange={handleChange}>
+                      <option value = 'none'> Select a race </option>
+                      {raceOptions.map((race) => {
+                          return(
+                              <option value={race.value}>
+                                  {race.label}
+                              </option>
+                          )
+                      })}
+                  </select>
 
-                    
-                    
-                        <Grid item xs={2} alignItems="stretch">
-                        <br /><br />
-                        <a style={{ color: 'white' }}>Race</a>
+                  <h1>
+                  {
+                      selectedRace != 'none'?
+                      <img src={imgMap[race.index]}></img>
+                      : null
+                  }   
+                  </h1>
+                </Grid>
+                  
+                <Grid item xs={30} sm={4}>
+                      {
+                          //if selectedValue is not none, show all attributes
+                          
+                          selectedRace !== 'none' ?  
+                          <RaceTextBox>
+                              <Box sx={{ maxWidth: "auto" }}>
+                                  <Tabs
+                                      value={value}
+                                      onChange={tabChange}
+                                      variant="scrollable"
+                                      scrollButtons
+                                      allowScrollButtonsMobile
+                                      aria-label="scrollable auto tabs example"
+                                  >
+                                      
+                                      <Tab label="Alignment" {...a11yProps(0)}/> 
+                                      <Tab label="Age" {...a11yProps(1)} />
+                                      <Tab label="Size" {...a11yProps(2)} />
+                                      <Tab label="Size Description" {...a11yProps(3)} />
+                                      <Tab label="Speed" {...a11yProps(4)} />
+                                      <Tab label="Languages" {...a11yProps(5)} />
+                                  </Tabs>
 
-                        <select value={selectedRace} onChange={handleChange}>
-                            <option value = 'none'> Select a race </option>
-                            {raceOptions.map((race) => {
-                                return(
-                                    <option value={race.value}>
-                                        {race.label}
-                                    </option>
-                                )
-                            })}
-                        </select>
-
-                    
-                        
-                        <br /><br />
-                        {
-                            //if selectedValue is not none, show all attributes
-                            
-                            selectedRace !== 'none' ?  
-                            <RaceTextBox>
-                                <Box sx={{ maxWidth: "auto" }}>
-                                    <Tabs
-                                        value={value}
-                                        onChange={tabChange}
-                                        variant="scrollable"
-                                        scrollButtons
-                                        allowScrollButtonsMobile
-                                        aria-label="scrollable auto tabs example"
-                                    >
-                                        
-                                        <Tab label="Alignment" {...a11yProps(0)}/> 
-                                        <Tab label="Age" {...a11yProps(1)} />
-                                        <Tab label="Size" {...a11yProps(2)} />
-                                        <Tab label="Size Description" {...a11yProps(3)} />
-                                        <Tab label="Speed" {...a11yProps(4)} />
-                                        <Tab label="Languages" {...a11yProps(5)} />
-                                    </Tabs>
-
-                                    <TabPanel value={value} index={0}>
-                                    {race.alignment}
-                                    </TabPanel>
-                                    <TabPanel value={value} index={1}>
-                                    {race.age}
-                                    </TabPanel>
-                                    <TabPanel value={value} index={2}>
-                                    {race.size}
-                                    </TabPanel>
-                                    <TabPanel value={value} index={3}>
-                                    {race.size_description}
-                                    </TabPanel>
-                                    <TabPanel value={value} index={4}>
-                                    {race.speed}
-                                    </TabPanel>
-                                    <TabPanel value={value} index={5}>
-                                    {race.language_desc}
-                                    </TabPanel>
-                                    </Box>
-                                
-                            </RaceTextBox>
-                            //else render nothing  
-                            : null
-                        }
-                        </Grid>
-                    
-                    </Grid>
-                </Box>
-            </Container>
-        </BuildTitle>
-    )
+                                  <TabPanel value={value} index={0}>
+                                  {race.alignment}
+                                  </TabPanel>
+                                  <TabPanel value={value} index={1}>
+                                  {race.age}
+                                  </TabPanel>
+                                  <TabPanel value={value} index={2}>
+                                  {race.size}
+                                  </TabPanel>
+                                  <TabPanel value={value} index={3}>
+                                  {race.size_description}
+                                  </TabPanel>
+                                  <TabPanel value={value} index={4}>
+                                  {race.speed}
+                                  </TabPanel>
+                                  <TabPanel value={value} index={5}>
+                                  {race.language_desc}
+                                  </TabPanel>
+                              </Box>
+                              
+                          </RaceTextBox>
+                          //else render nothing  
+                          : null
+                      }
+                  </Grid>
+                  
+              </Grid>
+            </Box>
+          </Container>
+      </BuildTitle>
+  )
 }
 
 export default RaceSelect;
